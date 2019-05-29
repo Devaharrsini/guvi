75a626ce5c98c34637fc24a393c7621b39230763
Devaharrsini <50878176+Devaharrsini@users.noreply.github.com>
2019-05-29 10:32:59 +0530

    Create greatest of three.py

---
+++ greatest of three.py
@@ -0,0 +1,13 @@
+num1,num2,num3=input().split()
+n1=int(num1)
+n2=int(num2)
+n3=int(num3)
+if(n1>n2):
+   if(n1>n3):
+     print(n1)
+   else:
+    print(n3)
+elif(n2>n3):
+  print(n2)
+else:
+  print(n3)
