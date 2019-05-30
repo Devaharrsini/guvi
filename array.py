0eb9b0184d95bcbf72a8cecab254657481ca5a58
Devaharrsini <50878176+Devaharrsini@users.noreply.github.com>
2019-05-30 22:19:47 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,14 @@
+intlist = list()
+n = int(input())
+k = int(input())
+
+sum = 0
+i=0
+while i < n :
+  t = int(input())
+  intlist. append(t)
+  if(i<k):
+    sum = sum + t
+  i = i+1
+
+print(sum)
